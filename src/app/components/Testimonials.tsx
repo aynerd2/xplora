68f62b9f0813c72243bf193f351bf095a4e24920
Ayobami <olawaleayowande@gmail.com>
2024-12-20 09:58:17 +0100

    Testimonial completed

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,36 +1,41 @@
-
+import { Avatar } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 
 const Testimonials = () => (
+  
+  <div className="flex flex-col items-center justify-center mb-32 gap-8">
+    <h1 className="text-white text-2xl font-graphik">TESTIMONIALS</h1>
 
-  <div className="flex flex-col items-center justify-center mb-16">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 w-full max-w-4xl px-4 sm:px-8 lg:px-16">
+    {[...Array(4)].map((_, index) => (
+      <div
+        key={index}
+        className={`flex flex-col w-full h-auto bg-[#040828] p-8 items-start gap-4 
+        border border-[#364250] rounded-lg`}
+      >
+        <div className="flex flex-row items-center gap-4">
+          <img
+            className="w-16 h-16 text-tiny rounded-full"
+            src={`https://i.pravatar.cc/150?u=a04258a2462d826712d-${index}`}
+          />
+          <div className="flex flex-col items-start gap-2">
+            <p className="text-xs text-white font-graphik">Claire Bell</p>
+            <p className="text-xs text-white font-graphik">Software Engineer</p>
+          </div>
+        </div>
 
-    <h1 className="text-white text-smdd font-graphik">TESTIMONIALS</h1>
-
-    <div className="grid grid-cols-4 gap-4">
-      <div className="w-1/3 h-1/3 bg-black p-8">
-        <p className="text-basic text-white font-graphik">Testimoni 1</p>
-        <p className="text-basic text-white font-graphik">this is a good company</p>
+        <p className="text-basic text-white font-graphik">
+          Slate helps you see how many more days you need to work to reach your
+          financial goal for the month and year. Slate helps you see how many
+          more days you need to work to reach your financial goal for the month
+          and year.
+        </p>
       </div>
-
-      <div className="w-1/3 h-1/3 bg-black p-8">
-        <p className="text-basic text-white font-graphik">Testimoni 1</p>
-        <p className="text-basic text-white font-graphik">this is a good company</p>
-      </div>
-
-      <div className="w-1/3 h-1/3 bg-black p-8">
-        <p className="text-basic text-white font-graphik">Testimoni 1</p>
-        <p className="text-basic text-white font-graphik">this is a good company</p>
-      </div>
-
-      <div className="w-1/3 h-1/3 bg-black p-8">
-        <p className="text-basic text-white font-graphik">Testimoni 1</p>
-        <p className="text-basic text-white font-graphik">this is a good company</p>
-      </div>
-    
-    </div>
-
+    ))}
   </div>
 
+
+  </div>
 );
 
 export default Testimonials;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,31 @@
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx}", // Include NextUI
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        primary: '#000319',
-        secondary: '#000319',
-        accent: '#000319',
+        primary: "#000319",
+        secondary: "#000319",
+        accent: "#000319",
       },
       fontFamily: {
-        graphik: ['Graphik', 'sans-serif'],
+        graphik: ["Graphik", "sans-serif"],
       },
       fontSize: {
-        custom: '16px', 
-        basic: '14px',  
-        smdd: '18px', 
-      }
-
+        custom: "16px",
+        basic: "14px",
+        smdd: "18px",
+      },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 } satisfies Config;
-
-
-
-// #000319
-// #040828
